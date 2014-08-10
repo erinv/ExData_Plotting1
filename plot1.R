e787d4444b552fa1765181acf66844706377f967
EEV <erinvaca@erin-vacas-imac.gateway.2wire.net>
2014-08-10 12:07:31 -0700

    Part 1 of Course Project 1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+
+
+
+##setwd("~/datasciencecoursera/Exploratory Data Analysis/power_consumption")
+##  Load data into R (assumes data file is in current working directory)
+
+> powerdata <- read.table("household_power_consumption.txt", header=TRUE, 
+                          sep=";", na.strings="?")
+
+#Concatenate date and time strings
+powerdata$unique <- paste(powerdata$Date, powerdata$Time)
+
+#Convert original Date variable to date class
+powerdata$Date <- as.Date(powerdata$Date, format = "%d/%m/%Y")
+
+#Select out rows for dates of interest
+subpower <- subset(powerdata, powerdata$Date=="2007-02-01" | powerdata$Date=="2007-02-02")
+
+#Convert "unique" variable to POSIXlt class
+subpower$unique <- strptime(subpower$unique, format = "%d/%m/%Y %H:%M:%S")
+
+#Open png driver, construct histogram, close driver/device to write file
+png(filename = "plot1.png", width=480, height=480)
+> hist(subpower$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+> dev.off()
